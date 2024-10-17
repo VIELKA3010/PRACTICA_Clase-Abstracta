b1a1419655c39623f19bf41ef304d33d32d0fc53
VIELKA3010 <143555915+VIELKA3010@users.noreply.github.com>
2024-10-17 12:32:49 -0600

    Add files via upload

---
+++ Controladora.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+
+public class Controladora {
+    public static void main(String[] args) {
+        
+        new Thread(new Reloj()).start();
+        
+        new Thread(new Ejecutor("calc", 0)).start();
+    }
+}
+
---
+++ Reloj.java
@@ -0,0 +1,43 @@
+
+
+import java.util.*;
+
+public class Reloj extends TareaPeriodica {
+
+    public Reloj() {
+        super(60);
+    }
+
+    public String leerHora() {
+        Calendar cal = new GregorianCalendar();
+        return cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
+    }
+
+    
+    public boolean necesitaEjecucion() {
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(ultimaEjecucion);
+        cal.add(Calendar.SECOND, periodo);
+        Calendar ahora = new GregorianCalendar();
+        return ahora.before(cal);
+    }
+
+
+    public int ejecutarTarea() {
+        Calendar c = new GregorianCalendar();
+        
+        System.out.println(String.format("%d:%02d:%02d", 
+            c.get(Calendar.HOUR_OF_DAY), 
+            c.get(Calendar.MINUTE), 
+            c.get(Calendar.SECOND)));
+        actualizar(); 
+        return 0; 
+	}
+	
+	public void run() {
+if (this.necesitaEjecucion()) {
+this.ejecutarTarea();
+    }
+ }
+
+}
